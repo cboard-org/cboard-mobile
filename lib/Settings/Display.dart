77febfe29ca361b32ed91fb5ba34498fca618e6e
nganphan123 <jill01009@gmail.com>
2021-02-08 23:30:43 -0800

    Fixed button bug

--- lib/Settings/Display.dart
+++ lib/Settings/Display.dart
@@ -27,16 +27,16 @@ class _MyStatefulWidget extends State<MyStatefulWidget>{
   bool _darkTheme = false;
   bool _predictiveText = false;
   bool _outputBar = false;
-  size uiSize = size.Standard;
-  size fontSize = size.Standard;
+  size _uiSize = size.Standard;
+  size _fontSize = size.Standard;
 
   @override
   Widget build(BuildContext context){
     return ListView(
           children: <Widget>[
-           radio_section('UI Size', 'Elements Size',uiSize),
+           radio_section('UI Size', 'Elements Size',_uiSize),
            Divider(height: 20, color: Color(0xffE0E0E0), thickness: 1,),
-           radio_section('Font Size', 'App Font Size',fontSize),
+           radio_section('Font Size', 'App Font Size',_fontSize),
            Divider(height: 20, color: Color(0xffE0E0E0), thickness: 1,),
            listTile_section('Enable Dark Theme', '...',_darkTheme),
            Divider(height: 20, color: Color(0xffE0E0E0), thickness: 1,),
@@ -55,20 +55,28 @@ class _MyStatefulWidget extends State<MyStatefulWidget>{
       value: state,
       onChanged: (bool newValue){
         setState(() {
-          state = newValue;
+          if(title=="Enable Dark Theme")
+            _darkTheme = newValue;
+          else if(title == "Enable Predictive Text")
+            _predictiveText = newValue;
+          else _outputBar = newValue;
         });
       },
     );
   }
 
-  Widget _radio (size groupState, size state){
+  Widget _radio (size groupState, size state, String title){
     return  Radio(
                   activeColor: Color(0xff5600E8),
                   value: state,
                   groupValue: groupState,
                   onChanged: (size newValue){
                     setState(() {
-                      groupState = newValue;
+                      if(title == 'UI Size')
+                        _uiSize = newValue;
+                      else{
+                        _fontSize = newValue;
+                      }
                     });
                   },
                 );
@@ -86,28 +94,19 @@ class _MyStatefulWidget extends State<MyStatefulWidget>{
                     Expanded(child: 
                     Column(
                       children:[
-                      _radio(groupState, size.Standard),
+                      _radio(groupState, size.Standard, title),
                       Text("Standard", style: TextStyle(fontSize: 12, color:Color(0xff7C7C7C)))]
                     )),
                     Expanded(child: 
                     Column(
                       children:[
-                       Radio(
-                  activeColor: Color(0xff5600E8),
-                  value: size.Large,
-                  groupValue: groupState,
-                  onChanged: (size newValue){
-                    setState(() {
-                      groupState = newValue;
-                    });
-                  },
-                ),
+                      _radio(groupState, size.Large, title),
                       Text("Large",style: TextStyle(fontSize: 12, color:Color(0xff7C7C7C)))]
                     )),
                     Expanded(child:
                     Column(
                       children:[
-                     _radio(groupState, size.ExtraLarge),
+                     _radio(groupState, size.ExtraLarge, title),
                       Text("Extra large", style: TextStyle(fontSize: 12, color:Color(0xff7C7C7C)))]
                     )),
                   ],),
