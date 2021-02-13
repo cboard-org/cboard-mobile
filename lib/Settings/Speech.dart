873072a274458317171c78e81d22c68fc6061648
nganphan123 <jill01009@gmail.com>
2021-02-13 10:40:23 -0800

    Center Speech title

--- lib/Settings/Speech.dart
+++ lib/Settings/Speech.dart
@@ -7,6 +7,7 @@ class Speech extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Speech'),
       ),
       body: Center(
