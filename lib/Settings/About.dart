d419f7b196d2598800310c0a8819dc876c66b98b
nganphan123 <jill01009@gmail.com>
2021-03-06 14:08:44 -0800

    Finish UI of About page

---
+++ lib/Settings/About.dart
@@ -0,0 +1,33 @@
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+class About extends StatelessWidget {
+  final controller = ScrollController();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('About'),
+      ),
+      body: FutureBuilder(
+          future: rootBundle.loadString("translation/About/en-US.md"),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData) {
+              return Markdown(
+                data: snapshot.data,
+                onTapLink: (text, url, title) {
+                  launch(url);
+                },
+              );
+            }
+
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+    );
+  }
+}
