2ac2b2c60aa058d95ebaaace5f6f5f778b484680
nganphan123 <jill01009@gmail.com>
2021-03-13 01:20:00 -0800

    Link Login/Signup button

--- lib/Settings/routes.dart
+++ lib/Settings/routes.dart
@@ -1,9 +1,10 @@
 import 'package:cboard_mobile/Settings/About.dart';
-import 'package:cboard_mobile/Settings/Feedback.dart';
+// import 'package:cboard_mobile/Settings/Feedback.dart';
 import 'package:cboard_mobile/Settings/Navigation.dart';
 import 'package:cboard_mobile/Settings/Speech.dart';
 import 'package:cboard_mobile/Settings/User_help.dart';
 
+import '../onboarding/screens/welcome.dart';
 import 'Display.dart';
 
 getRoute() {
@@ -13,5 +14,6 @@ getRoute() {
     '/navigation': (context) => Navigation(),
     '/user help': (context) => User_help(),
     '/about': (context) => About(),
+    '/welcome': (context) => Welcome(),
   };
 }
