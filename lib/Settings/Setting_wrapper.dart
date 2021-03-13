7d8afbee89529c61d669d5f7e5dae400fa58f1af
nganphan123 <jill01009@gmail.com>
2021-03-13 01:19:22 -0800

    Change button format to shared property

--- lib/Settings/Setting_wrapper.dart
+++ lib/Settings/Setting_wrapper.dart
@@ -2,43 +2,15 @@ import 'package:cboard_mobile/Settings/routes.dart';
 import 'package:cboard_mobile/shared/app-bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import '../shared/button.dart';
 import 'routes.dart';
 
 class Setting_wrapper extends StatelessWidget {
   // ThemeNotifier theme = new ThemeNotifier();
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color(0xFF23036A),
-        accentColor: Color(0xFFE3CAFF),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: Scaffold(
-        backgroundColor: Color(0xffE5E5E5),
-        appBar: BaseAppBar(
-          title: Text('Settings'),
-          appBar: AppBar(),
-        ),
-        body: MyStatefulWidget(),
-      ),
-      routes: getRoute(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-  @override
-  _MyStatefulWidget createState() => _MyStatefulWidget();
-}
-
-class _MyStatefulWidget extends State<MyStatefulWidget> {
   final sections = ['People', 'Language', 'System', 'Help'];
   final List<Map<String, List<Object>>> features = <Map<String, List<Object>>>[
     {
-      'Guest': [Icons.account_circle]
+      'Guest': [Icons.account_circle, '/welcome']
     },
     {
       'Language': [Icons.language, '/speech'],
@@ -60,69 +32,98 @@ class _MyStatefulWidget extends State<MyStatefulWidget> {
   ];
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      separatorBuilder: (BuildContext context, int index) {
-        return SizedBox(
-          height: 10,
-        );
-      },
-      itemCount: sections.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          padding: EdgeInsets.all(16.0),
-          decoration: new BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(5.0)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                sections[index],
-                textAlign: TextAlign.left,
-              ),
-              ListView.separated(
-                  separatorBuilder: (BuildContext context, int index) =>
-                      Divider(color: Color(0xffE0E0E0), indent: 78),
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: features[index].length,
-                  itemBuilder: (BuildContext context, int iconIndex) {
-                    return GestureDetector(
-                      onTap: index != 0
-                          ? () {
-                              Navigator.of(context).pushNamed(
-                                features[index].values.elementAt(iconIndex)[1],
-                              );
-                            }
-                          : null,
-                      child: ListTile(
-                        leading: Icon(
-                          features[index].values.elementAt(iconIndex)[0],
-                          color: Color(0xff391E75),
-                          size: 20,
-                        ),
-                        title: Text(features[index].keys.elementAt(iconIndex)),
-                        trailing: (index == 0)
-                            ? Container(
-                                width: 121,
-                                height: 27,
-                                alignment: Alignment.center,
-                                decoration: new BoxDecoration(
-                                    border: Border.all(
-                                        width: 1.0,
-                                        color: const Color(0xff6200EE)),
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(100.0)),
-                                child: Text('LOGIN/SIGN UP',
-                                    style: TextStyle(color: Color(0xff391E75))),
-                              )
-                            : null,
-                      ),
-                    );
-                  }),
-            ],
+    return MaterialApp(
+      routes: getRoute(),
+      theme: ThemeData(
+        primaryColor: Color(0xFF23036A),
+        accentColor: Color(0xFFE3CAFF),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: Scaffold(
+          backgroundColor: Color(0xffE5E5E5),
+          appBar: BaseAppBar(
+            title: Text('Settings'),
+            appBar: AppBar(),
           ),
-        );
-      },
+          body: ListView.separated(
+            separatorBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                height: 10,
+              );
+            },
+            itemCount: sections.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: new BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5.0)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      sections[index],
+                      textAlign: TextAlign.left,
+                    ),
+                    ListView.separated(
+                        separatorBuilder: (BuildContext context, int index) =>
+                            Divider(color: Color(0xffE0E0E0), indent: 78),
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: features[index].length,
+                        itemBuilder: (BuildContext context, int iconIndex) {
+                          return GestureDetector(
+                            onTap: index != 0
+                                ? () {
+                                    Navigator.of(context).pushNamed(
+                                      features[index]
+                                          .values
+                                          .elementAt(iconIndex)[1],
+                                    );
+                                  }
+                                : null,
+                            child: ListTile(
+                              leading: Icon(
+                                features[index].values.elementAt(iconIndex)[0],
+                                color: Color(0xff391E75),
+                                size: 20,
+                              ),
+                              title: Text(
+                                  features[index].keys.elementAt(iconIndex)),
+                              trailing: (index == 0)
+                                  ? Container(
+                                      width: 121,
+                                      height: 27,
+                                      alignment: Alignment.center,
+                                      decoration: new BoxDecoration(
+                                          border: Border.all(
+                                              width: 1.0,
+                                              color: const Color(0xff6200EE)),
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(100.0)),
+                                      child: Button(
+                                        label: Text('LOGIN/SIGN UP'),
+                                        isPrimary: false,
+                                        onPress: () {
+                                          Navigator.of(context).pushNamed(
+                                            features[index]
+                                                .values
+                                                .elementAt(iconIndex)[1],
+                                          );
+                                        },
+                                      ),
+                                    )
+                                  : null,
+                            ),
+                          );
+                        }),
+                  ],
+                ),
+              );
+            },
+          )),
     );
   }
 }
