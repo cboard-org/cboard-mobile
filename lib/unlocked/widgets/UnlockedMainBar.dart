9cb43b0d90e971aa8871e912e388067cf8174222
Akanksha1212 <singhakanksha221b@gmail.com>
2021-10-09 21:40:41 +0530

    minor change

--- lib/unlocked/widgets/UnlockedMainBar.dart
+++ lib/unlocked/widgets/UnlockedMainBar.dart
@@ -1,157 +1,3 @@
-<<<<<<< HEAD
-import 'package:cboard_mobile/stylesheets/constants.dart';
-import 'package:cboard_mobile/unlocked/ManageBoards.dart';
-import 'package:cboard_mobile/unlocked/NewBoard.dart';
-import 'package:flutter/material.dart';
-
-class UnlockedMainBar extends StatelessWidget {
-  // final double scrollOffset;
-
-  // const UnlockedMainBar({
-  //   Key key,
-  //   this.scrollOffset = 0,
-  // }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: 10.0,
-            horizontal: 10.0,
-          ),
-          color: fog,
-          child: Row(
-            children: [
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FlatButton(
-                      onPressed: () {
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return Wrap(
-                              children: [
-                                ListTile(
-                                  leading: Icon(
-                                    Icons.folder,
-                                    color: paua,
-                                  ),
-                                  title: Text('Manage Boards'),
-                                  onTap: () => {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                ManageBoardsScreen()))
-                                  },
-                                ),
-                                ListTile(
-                                  leading: Icon(
-                                    Icons.create_new_folder,
-                                    color: paua,
-                                  ),
-                                  title: Text('New Board'),
-                                  onTap: () => {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => NewBoard()))
-                                  },
-                                ),
-                                ListTile(
-                                  leading: Icon(
-                                    Icons.assignment,
-                                    color: paua,
-                                  ),
-                                  title: Text('Board 2'),
-                                ),
-                                ListTile(
-                                  leading: Icon(
-                                    Icons.assignment,
-                                    color: paua,
-                                  ),
-                                  title: Text('Board 2'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      // padding: EdgeInsets.all(10.0),
-                      child: Row(
-                        // Replace with a Row for horizontal icon + text
-                        children: <Widget>[
-                          Text(
-                            'Board Name',
-                            style: TextStyle(
-                                color: paua,
-                                fontSize: 16.0,
-                                fontFamily: "Robotto",
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Icon(Icons.arrow_drop_down),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () =>
-                              print('Create'), // insert new screen here
-                          child: Icon(
-                            Icons.create,
-                            color: paua,
-                            size: 20.0,
-                          ),
-                        ),
-                        SizedBox(width: 15),
-                        GestureDetector(
-                          onTap: () =>
-                              print('Delete'), // insert new screen here
-                          child: Icon(
-                            Icons.delete,
-                            color: paua,
-                            size: 20.0,
-                          ),
-                        ),
-                        // Setting
-                        SizedBox(width: 15),
-                        GestureDetector(
-                          onTap: () =>
-                              print('Settings'), // insert new screen here
-                          child: Icon(
-                            Icons.settings,
-                            color: paua,
-                            size: 20.0,
-                          ),
-                        ),
-                        SizedBox(width: 15),
-                        GestureDetector(
-                          onTap: () => print('Lock'), // insert new screen here
-                          child: Icon(
-                            Icons.lock_open,
-                            color: paua,
-                            size: 20.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-=======
 import 'package:cboard_mobile/data/data.dart';
 import 'package:cboard_mobile/lockedScreen/screens/home_screen.dart';
 import 'package:cboard_mobile/models/home.dart';
@@ -241,7 +87,10 @@ class UnlockedMainBar extends StatelessWidget {
                             fontFamily: "Robotto",
                             fontWeight: FontWeight.bold),
                       ),
-                      Icon(Icons.arrow_drop_down,color: Theme.of(context).primaryColor,),
+                      Icon(
+                        Icons.arrow_drop_down,
+                        color: Theme.of(context).primaryColor,
+                      ),
                     ],
                   ),
                 ),
@@ -279,8 +128,14 @@ class UnlockedMainBar extends StatelessWidget {
                     GestureDetector(
                       onTap: () {
                         /// locked to unlocked transition
-                        Provider.of<HomeModel>(context,listen: false).resetLock();
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(data: example,)));
+                        Provider.of<HomeModel>(context, listen: false)
+                            .resetLock();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen(
+                                      data: example,
+                                    )));
                       }, // insert new screen here
                       child: Icon(
                         Icons.lock_open,
@@ -298,4 +153,3 @@ class UnlockedMainBar extends StatelessWidget {
     );
   }
 }
->>>>>>> origin/Summer2021
